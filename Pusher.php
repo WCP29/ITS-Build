f6a48c97c10d47a89e1be1b347fafa0e9c617e4f
grinbergjeff <grinbergjeff@gmail.com>
2016-02-11 03:57:26 +0000

    Got PHP code to connect with custom mySQL database!

--- Pusher.php
+++ Pusher.php
@@ -13,4 +13,25 @@
    /* $directions = json_decode($_POST['map']);
     var_dump($directions);
     */
+  
+  //Code from C9 community:
+  
+  //Connect to the database
+    $host = "127.0.0.1";
+    $user = "grinbergjeff";                     //Your Cloud 9 username
+    $pass = "";                                  //Remember, there is NO password by default!
+    $db = "WCP29";                                  //Your database name you want to connect to
+    $port = 3306;                                //The port #. It is always 3306
+    
+    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+
+
+
+    //And now to perform a simple query to make sure it's working
+    $query = "SELECT * FROM buildings";
+    $result = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "The Building Name is: " . $row['name'] . " and the ID is: " . $row['id'];
+    }
 ?>
